362b0edd9cbb370cbd79db73c35a5477a2688632
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-08-15 12:58:02 +0500

    edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import Image from 'next/image'
+"use client";
+import { useState } from 'react';
+import { CldImage } from 'next-cloudinary';
+import { CldUploadButton } from 'next-cloudinary';
+
+type UploadResult = {
+  info: {
+    public_id: string;
+  };
+  event: "success";
+};
 
 export default function Home() {
+  const [imageId, setImageId] = useState("")
   return (
     <div>
-      hello
+      <CldUploadButton
+        onUpload={(result: UploadResult) => {
+          setImageId(result.info.public_id);
+        }}
+        uploadPreset="g7hq9orw" />
+
+      {imageId && (
+        <CldImage
+        width="960"
+        height="600"
+        src={imageId}
+        sizes="100vw"
+        alt="Description of my image"
+      />
+      )
+      }
     </div>
   )
 }
